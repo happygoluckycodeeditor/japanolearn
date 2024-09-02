d9e97aef04930d08c5493a07b83889e2844c5506
TanmayB <tanmay.bagwe.tb@gmail.com>
2024-09-02 23:34:43 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-4xl font-bold">JapanoLearn</h1>
+      <button className="btn btn-primary mt-4">Get Started</button>
+    </div>
+  );
+}
+
